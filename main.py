64f79253f6af5072d78911d5e3634ac926b8a988
Ayushmak02 <83110177+Ayushmak02@users.noreply.github.com>
2024-05-07 12:29:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+
 import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as gen_ai
+
 
 # Load environment variables
 load_dotenv()
@@ -33,29 +35,6 @@ if "chat_session" not in st.session_state:
     st.session_state.chat_session = model.start_chat(history=[])
 
 
-# Function to check if a prompt adheres to the guidelines
-def follows_guidelines(prompt):
-    # Define the guidelines
-    guidelines = """
-    You are NeuroScan, an automated chatbot for people wanting to know about ADHD(Attention Deficit Hyperactivity Disorder). 
-    You first greet the user, then ask them if they have ADHD or not, and then asks what do they want to learn about ADHD. 
-    You answer their questions, or help them if they need help about any topic on ADHD then summarize it and check for a final time if the user wants to ask anything else. 
-    If they ask , you answer them again. 
-    Finally you thank them for using the chatbot.
-    You respond in a short, very conversational friendly style.
-    """
-
-    # Check if the prompt follows the guidelines
-    # # You can customize this based on your specific guidelines
-    # if "neuroscience" in prompt.lower():
-    #     if len(prompt.split()) <= 20:  # Example: Limiting the length of the question to 20 words
-    #         return True, guidelines
-    #     else:
-    #         return False, "Your question is too long. Please limit it to a few sentences."
-    # else:
-    #     return False, "Please ask a question related to neurosciences."
-
-
 # Display the chatbot's title on the page
 st.title("NeuroScan Prompt")
 
@@ -65,21 +44,14 @@ for message in st.session_state.chat_session.history:
         st.markdown(message.parts[0].text)
 
 # Input field for user's message
-user_prompt = st.text_area("Ask Gemini-Pro...")
+user_prompt = st.chat_input("Ask Gemini-Pro...")
 if user_prompt:
-    # Check if the prompt follows the guidelines
-    follows, response = follows_guidelines(user_prompt)
-    if follows:
-        # Add user's message to chat and display it
-        st.chat_message("user").markdown(user_prompt)
+    # Add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
 
-        # Send user's message to Gemini-Pro and get the response
-        gemini_response = st.session_state.chat_session.send_message(user_prompt)
+    # Send user's message to Gemini-Pro and get the response
+    gemini_response = st.session_state.chat_session.send_message(user_prompt)
 
-        # Display Gemini-Pro's response
-        with st.chat_message("assistant"):
-            st.markdown(gemini_response.text)
-    else:
-        # Prompt the user with the guideline response
-        with st.chat_message("assistant"):
-            st.markdown(response)
+    # Display Gemini-Pro's response
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response.text)
